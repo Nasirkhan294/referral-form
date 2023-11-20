6937566a5966e4e3ca30d86a2183e10a915d06f7
Nasir <nasirbhatti.nk@gmail.com>
2023-11-20 18:39:16 +0500

    Add form content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { TextField } from "@mui/material";
+
 function App() {
   return (
-    <div>
-     <h1>Hello React!!!</h1>
-    </div>
+    <>
+      <header className="formHeader">
+        <p>
+          <b>Patient Referral Form</b> <br />
+          Hayes Valley Health San Francisco
+        </p>
+      </header>
+      <Form className="form">
+        <div className="formDiv">
+          <p>Referral Patient</p>
+          <p>You can add up to five patients at a time</p>
+        </div>
+        <Container>
+          <p>New Referral</p>
+          <Row className="formRow">
+            <Col>
+              <TextField
+                variant="standard"
+                label="First Name"
+                className="textField"
+              />
+              <TextField
+                variant="standard"
+                label="Date of Birth"
+                className="textField"
+              />
+              <TextField
+                variant="standard"
+                label="Phone"
+                className="textField"
+              />
+            </Col>
+            <Col>
+              <TextField
+                variant="standard"
+                label="Last Name"
+                className="textField"
+              />
+              <TextField
+                variant="standard"
+                label="Contact Language"
+                className="textField"
+              />
+              <TextField
+                variant="standard"
+                label="Email"
+                className="textField"
+              />
+            </Col>
+            <TextField variant="standard" label="Address" />
+            <TextField variant="standard" label="Notes/Reason" />
+          </Row>
+        <Button variant="primary" type="submit" className="btn">
+          Click here to submit form
+        </Button>
+        </Container>
+      </Form>
+    </>
   );
 }
 
